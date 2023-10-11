2b7d4cb0b1b3ad95a99b32f6a2541d642d63c913
Plague <74983533+PlagueFPS@users.noreply.github.com>
2023-10-10 21:21:26 -0400

    `QuestDetailsLoader` complete

---
+++ app/quests/[id]/loading.tsx
@@ -0,0 +1,5 @@
+import QuestDetailsLoader from '@/components/Loaders/QuestsLoader/QuestDetailsLoader/QuestDetailsLoader'
+
+export default function loading() {
+  return <QuestDetailsLoader />
+}
--- components/Loaders/QuestsLoader/QuestDetailsLoader/QuestDetailsLoader.tsx
+++ components/Loaders/QuestsLoader/QuestDetailsLoader/QuestDetailsLoader.tsx
@@ -5,6 +5,15 @@ export default function QuestDetailsLoader() {
   return (
     <>
       <HeaderLoader />
+      <div className={ styles.container }>
+        <div className={ styles.questContainer }>
+          <div className={ styles.title } />
+          <div className={ styles.description } />
+        </div>
+        <div className={ styles.tasksContainer } />
+        <div className={ styles.taskItems_Container } />
+        <div className={ styles.rewardsContainer }  />
+      </div>
     </>
   )
 }

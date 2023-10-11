d04b74d21f3993596640a37e5fc3e2fbe1dffc8c
Plague <74983533+PlagueFPS@users.noreply.github.com>
2023-10-11 15:46:35 -0400

    Fixed `useCycleState` bug

--- hooks/useCycleState.tsx
+++ hooks/useCycleState.tsx
@@ -1,25 +1,21 @@
 "use client"
+import { DataTypes } from "@/types/DataTypes"
 
-interface Data {
-  inGameName: string
-  key?: string
-}
-
-export default function useCycleState<T extends Data>(staticData: T[], currentData: T, setCurrentData: React.Dispatch<React.SetStateAction<T>>) {
+export default function useCycleState<T extends DataTypes>(staticData: T[], currentState: T, setCurrentState: React.Dispatch<React.SetStateAction<T>>) {
   const cyclePrevState = () => {
-    const index = staticData.findIndex(data => data.inGameName === currentData.inGameName || data.key === currentData.key)
+    const index = staticData.findIndex(data => 'key' in data && 'key' in currentState ? data.key === currentState.key : data.inGameName === currentState.inGameName)
     const newIndex = index - 1
 
-    if (newIndex < 0) setCurrentData(staticData[staticData.length - 1])
-    else return setCurrentData(staticData[newIndex])
+    if (newIndex < 0) setCurrentState(staticData[staticData.length - 1])
+    else return setCurrentState(staticData[newIndex])
   }
 
   const cycleNextState = () => {
-    const index = staticData.findIndex(data => data.inGameName === currentData.inGameName || data.key === currentData.key)
+    const index = staticData.findIndex(data => 'key' in data && 'key' in currentState ? data.key === currentState.key : data.inGameName === currentState.inGameName)
     const newIndex = index + 1
 
-    if (newIndex < staticData.length) setCurrentData(staticData[newIndex])
-    else return setCurrentData(staticData[0])
+    if (newIndex < staticData.length) setCurrentState(staticData[newIndex])
+    else return setCurrentState(staticData[0])
   }
 
   return { cyclePrevState, cycleNextState }
